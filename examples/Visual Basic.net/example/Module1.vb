921c2c34ff63ee6d9eceeb91c3ecd98af5bbfc21
foen <foen1337@gmail.com>
2018-01-15 10:14:43 +0100

    Visual Basic.net
    visual basic.net example

---
+++ examples/Visual Basic.net/example/Module1.vb
@@ -0,0 +1,11 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Bl3p As New Bl3p_PrivateApi With {.publickey = "", .privatekey = ""}
+        ' Console.WriteLine(Bl3p.walletHistory("LTC", 25))
+        'Console.WriteLine(Bl3p.getLastDepositAddress("LTC"))
+        Console.WriteLine(Bl3p.fullDepth("LTC"))
+        Console.ReadKey()
+    End Sub
+
+End Module
